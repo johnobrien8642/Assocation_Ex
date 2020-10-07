597b46f7960bf2b6194e5a7aac4d52e25a2981fb
your name here <johnobrien8642@gmail.com>
2020-10-07 10:34:20 -0400

    ::Course, ::User, #has_many and #has_many_through methods created

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,11 @@
 class Course < ApplicationRecord
 
+    has_many :enrollments,
+      class_name: :Enrollment,
+      foreign_key: :course_id,
+      primary_key: :id
+
+    has_many :enrolled_students,
+      through: :enrollments,
+      source: :user
 end
